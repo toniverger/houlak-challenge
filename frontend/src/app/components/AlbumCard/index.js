f8389e5d91aca8c695dba77f73e00e2fb9d85a48
toniverger <toniverger@msn.com>
2023-11-18 13:24:11 -0300

    frontend setup with Material UI

---
+++ frontend/src/app/components/AlbumCard/index.js
@@ -0,0 +1,36 @@
+"use client";
+import styles from "./index.module.css";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+
+export default function AlbumCard() {
+  return (
+    <Card sx={{ maxWidth: 345, marginBottom: 10 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
