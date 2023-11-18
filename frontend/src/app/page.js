def9a067315c45fcc4215bc98d2239b1ef9cc8ac
toniverger <toniverger@msn.com>
2023-11-18 19:05:12 -0300

    add dark-light themes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,27 @@
 "use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 import AppBar from "./components/TopBar";
 import SearchBar from "./components/SearchBar";
 import AlbumCard from "./components/AlbumCard";
 import Grid from "@mui/material/Grid";
+import { ThemeProvider } from "@mui/material/styles";
+import { lightTheme, darkTheme } from "./theme";
+import { CssBaseline } from "@mui/material";
 
 export default function Home() {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme.palette.mode === "light" ? darkTheme : lightTheme
+    );
+  };
+
   return (
-    <main>
-      <AppBar />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar theme={theme} toggleTheme={toggleTheme} />
       <SearchBar />
       <div className={styles.gridContainer}>
         <Grid container spacing={2}>
@@ -29,6 +42,6 @@ export default function Home() {
           </Grid>
         </Grid>
       </div>
-    </main>
+    </ThemeProvider>
   );
 }
